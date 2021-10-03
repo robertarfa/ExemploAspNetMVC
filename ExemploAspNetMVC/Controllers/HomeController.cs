9d0d005b5f7b8be8b9f5a1aca5145655294f1f0f
rober <rober@LAPTOP-DH58BOM9>
2021-10-02 23:57:19 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploAspNetMVC/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using ExemploAspNetMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Recode.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploAspNetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cursos()
+        {
+            return View(cursos);
+        }
+
+        private static IList<Curso> cursos = new List<Curso>()
+        {
+            new Curso()
+            {
+                ID = 1,
+                Descricao = "Asp Net MVC",
+                CargaHoraria = 40
+            },
+                new Curso()
+            {
+                ID = 2,
+                Descricao = "Banco de dados",
+                CargaHoraria = 30
+            },
+                    new Curso()
+            {
+                ID = 3,
+                Descricao = "React",
+                CargaHoraria = 60
+            },
+                        new Curso()
+            {
+                ID = 4,
+                Descricao = "HTML e CSS",
+                CargaHoraria = 20
+            },
+
+        };
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
